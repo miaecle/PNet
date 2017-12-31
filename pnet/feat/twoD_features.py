657f8e11689576453f2e5341b2df4b518122b89a
miaecle <zqwu@stanford.edu>
2017-12-30 17:02:12 -0800

    update

--- pnet/feat/twoD_features.py
+++ pnet/feat/twoD_features.py
@@ -63,43 +63,45 @@ def mutual_information(dataset):
   for i in range(dataset.n_samples):  
     if os.path.exists(data_dir+'/MI_ALL/'+dataset._IDs[i]+'.mi'):
       continue
-    msa_path = form_msa(dataset.select_by_index(i))
-    with open(msa_path, 'r') as f:
-      sequences = [[MapToID(res.upper()) for res in record.seq.tostring()] for record in Bio.SeqIO.parse(f, 'fasta')]
-    with open(msa_path, 'r') as f:
-      for first_record in Bio.SeqIO.parse(f, 'fasta'):
-        break
-    index = [i for i, res in enumerate(first_record.seq.tostring()) if res != '-']
-    sequences = np.array(sequences)[:, index]
-  
-    num_res = len(index)
-    feed_dict = {n_res: num_res}
-    out_ct_1D = np.zeros((num_res, 21))
-    out_ct_2D = np.zeros((num_res, num_res, 21, 21))
-  
-    n_batch = sequences.shape[0]//20+1
-    for j in range(n_batch):
-      if j==n_batch-1:
-        feed_dict[inputs] = sequences[j*20:, :]
-      else:
-        feed_dict[inputs] = sequences[j*20:(j+1)*20, :]
-      out_ct_1D += sess.run(ct_1D, feed_dict=feed_dict)
-      out_ct_2D += sess.run(ct_2D, feed_dict=feed_dict)
-    freq_1D = out_ct_1D/sequences.shape[0]
-    freq_2D = np.reshape(out_ct_2D, (num_res, num_res, 21*21))/sequences.shape[0]
+    try:
+      msa_path = form_msa(dataset.select_by_index(i))
+      with open(msa_path, 'r') as f:
+        sequences = [[MapToID(res.upper()) for res in record.seq.tostring()] for record in Bio.SeqIO.parse(f, 'fasta')]
+      with open(msa_path, 'r') as f:
+        for first_record in Bio.SeqIO.parse(f, 'fasta'):
+          break
+      index = [i for i, res in enumerate(first_record.seq.tostring()) if res != '-']
+      sequences = np.array(sequences)[:, index]
     
-    H_1D = np.sum(-freq_1D * np.log(freq_1D + 1e-7) / np.log(21), axis=1)
-    H_2D = np.sum(-freq_2D * np.log(freq_2D + 1e-7) / np.log(21), axis=2)
+      num_res = len(index)
+      feed_dict = {n_res: num_res}
+      out_ct_1D = np.zeros((num_res, 21))
+      out_ct_2D = np.zeros((num_res, num_res, 21, 21))
     
-    MI = -H_2D + np.stack([H_1D]*num_res, axis=1) + np.stack([H_1D]*num_res, axis=0)
-    
-    MI_1D = (np.sum(MI, axis=1) - np.diag(MI))/(num_res-1)
-    MI_av = (np.sum(MI) - np.sum(np.diag(MI)))/num_res/(num_res-1)
-    APC = np.stack([MI_1D]*num_res, axis=1)*np.stack([MI_1D]*num_res, axis=0)/MI_av
-    normalized_MI = MI - APC
-    
-    joblib.dump([freq_1D, freq_2D, MI, normalized_MI], data_dir+'/MI_ALL/'+dataset._IDs[i]+'.mi')
-  
+      n_batch = sequences.shape[0]//20+1
+      for j in range(n_batch):
+        if j==n_batch-1:
+          feed_dict[inputs] = sequences[j*20:, :]
+        else:
+          feed_dict[inputs] = sequences[j*20:(j+1)*20, :]
+        out_ct_1D += sess.run(ct_1D, feed_dict=feed_dict)
+        out_ct_2D += sess.run(ct_2D, feed_dict=feed_dict)
+      freq_1D = out_ct_1D/sequences.shape[0]
+      freq_2D = np.reshape(out_ct_2D, (num_res, num_res, 21*21))/sequences.shape[0]
+      
+      H_1D = np.sum(-freq_1D * np.log(freq_1D + 1e-7) / np.log(21), axis=1)
+      H_2D = np.sum(-freq_2D * np.log(freq_2D + 1e-7) / np.log(21), axis=2)
+      
+      MI = -H_2D + np.stack([H_1D]*num_res, axis=1) + np.stack([H_1D]*num_res, axis=0)
+      
+      MI_1D = (np.sum(MI, axis=1) - np.diag(MI))/(num_res-1)
+      MI_av = (np.sum(MI) - np.sum(np.diag(MI)))/num_res/(num_res-1)
+      APC = np.stack([MI_1D]*num_res, axis=1)*np.stack([MI_1D]*num_res, axis=0)/MI_av
+      normalized_MI = MI - APC
+      
+      joblib.dump([freq_1D, freq_2D, MI, normalized_MI], data_dir+'/MI_ALL/'+dataset._IDs[i]+'.mi')
+    except:
+      continue
 
 
   
